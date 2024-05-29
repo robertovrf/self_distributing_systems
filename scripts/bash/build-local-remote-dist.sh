465753dae4bb3597a33b3107ea273f95df6b635d
igormandello <igormandello@gmail.com>
2024-05-29 17:41:59 -0300

    feat: adds local remote-dist shell

---
+++ scripts/bash/build-local-remote-dist.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pushd "$(dirname "$(readlink -f "$0")")"
+cd ../../
+docker build -t remote-dist:all -f scripts/docker/remote_dist/Dockerfile .
+docker tag remote-dist:all robertovrf/remote-dist:all
+kind load docker-image robertovrf/remote-dist:all
